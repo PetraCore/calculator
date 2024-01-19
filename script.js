6a47ecfaaf631558f9b24a44dc58db8bcfda9627
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-19 18:21:23 +0100

    Add a central function that calls the operations

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operate(operator, x, y) {
+    switch(operator) {
+        case '+': {
+            return add(x, y);
+            break;
+        }
+        case '-': {
+            return subtract(x, y);
+            break;
+        }
+        case '*': {
+            return multiply(x, y);
+            break;
+        }
+        case '/': {
+            return divide(x, y);
+            break;
+        }
+    }
+}
