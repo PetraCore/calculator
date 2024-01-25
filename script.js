0457c97f9123177c988858cf56c7ca9a504ffbb4
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-01-25 15:13:46 +0100

    Add functionality to operator buttons

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const DISPLAY = document.querySelector('.display');
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = undefined;
+let secondNumber = undefined;
 let operator = '';
+let isDisplayingResult = false;
 
-function add(x, y) {
+function add(x, y = 0) {
     return x + y;
 }
 
-function subtract(x, y) {
+function subtract(x, y = 0) {
     return x - y;
 }
 
-function multiply(x, y) {
+function multiply(x, y = 1) {
     return x * y;
 }
 
-function divide(x, y) {
+function divide(x, y = 1) {
     return x / y;
 }
 
@@ -38,6 +39,7 @@ function operate(operator, x, y) {
             break;
         }
     }
+    return x;
 }
 
 function updateDisplay(event, content) {
@@ -45,32 +47,108 @@ function updateDisplay(event, content) {
         content = event.target.textContent;
     }
     if (content) {
-        DISPLAY.textContent += content;
+        if (isDisplayingResult) {
+            DISPLAY.textContent = '';
+            isDisplayingResult = false;
+        }
+
+        if(Number(DISPLAY.textContent) === 0) {
+            DISPLAY.textContent = content;
+        } else {
+            DISPLAY.textContent += content;
+        }
     }
 }
 
 function clear() {
-    DISPLAY.textContent = '';
-    firstNumber = 0;
-    secondNumber = 0;
-    operator = '';
+    DISPLAY.textContent = '0';
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = '+';
 }
 
 function clearEntry() {
-    DISPLAY.textContent = '';
+    if(!isDisplayingResult) {
+        DISPLAY.textContent = '0';
+    }
+}
+
+function setNumberFromDisplay() {
+    if (!firstNumber) {
+        firstNumber = Number(DISPLAY.textContent);
+    } else {
+        secondNumber = Number(DISPLAY.textContent);
+    }
+}
+
+function calculate(op) {
+    if (!isDisplayingResult) {
+        firstNumber = operate(operator, firstNumber, secondNumber);
+    }
+
+    DISPLAY.textContent = firstNumber;
+    operator = op;
+    isDisplayingResult = true;
+}
+
+function evaluate() {
+    if (!isDisplayingResult) {
+        setNumberFromDisplay();
+    }
+
+    firstNumber = operate(operator, firstNumber, secondNumber);
+    DISPLAY.textContent = firstNumber;
+    isDisplayingResult = true;
+}
+
+function negate() {
+    DISPLAY.textContent = -Number(DISPLAY.textContent);
+    if (isDisplayingResult) {
+        firstNumber = -firstNumber;
+    }
 }
 
 function initializeButtons() {
-    let digitBtns = document.querySelectorAll('.btn.digit');
+    const digitBtns = document.querySelectorAll('.btn.digit');
     digitBtns.forEach((digitBtn) => {
         digitBtn.addEventListener('click', updateDisplay);
     });
 
-    let cBtn = document.querySelector('#c');
+    const cBtn = document.querySelector('#c');
     cBtn.addEventListener('click', clear);
 
-    let ceBtn = document.querySelector('#ce');
+    const ceBtn = document.querySelector('#ce');
     ceBtn.addEventListener('click', clearEntry);
+
+    const addBtn = document.querySelector('#add');
+    addBtn.addEventListener('click', () => {
+        setNumberFromDisplay();
+        calculate('+');
+    });
+
+    const subtractBtn = document.querySelector('#subtract');
+    subtractBtn.addEventListener('click', () => {
+        setNumberFromDisplay();
+        calculate('-');
+    });
+
+    const multiplyBtn = document.querySelector('#multiply');
+    multiplyBtn.addEventListener('click', () => {
+        setNumberFromDisplay();
+        calculate('*');
+    });
+
+    const divideBtn = document.querySelector('#divide');
+    divideBtn.addEventListener('click', () => {
+        setNumberFromDisplay();
+        calculate('/');
+    });
+
+    const evaluateBtn = document.querySelector('#evaluate');
+    evaluateBtn.addEventListener('click', evaluate);
+
+    const negateBtn = document.querySelector('#negate');
+    negateBtn.addEventListener('click', negate);
 }
 
 initializeButtons();
